09584b88343fc9d947a6d0ecc1e62d5df3d96099
afeef-rb <112685084+afeef-rb@users.noreply.github.com>
2023-04-12 13:40:44 +0530

    Update incident-no_loggedin-user.js

--- incident-no_loggedin-user.js
+++ incident-no_loggedin-user.js
@@ -3,10 +3,10 @@
 When any incident record is opened, when the form loads, show an info message about the current incident number and the logged in user
 **/
 
-1. All > System Definition > Client Scripts : New, OR
-2. Incident List > List context menu > Configure > Client Scripts 
-3. Table: [incident], Type: onLoad
-4. Script:
+// 1. All > System Definition > Client Scripts : New, OR
+// 2. Incident List > List context menu > Configure > Client Scripts 
+// 3. Table: [incident], Type: onLoad
+// 4. Script:
 
 function onLoad() {
    var recordNumber = g_form.getValue('number');
@@ -14,5 +14,5 @@ function onLoad() {
    g_form.addInfoMessage("The incident number is " + recordNumber + " and the logged in user is " + userFullName);
 }
 
-5. All > Incident > All : Click on an Incident Form
-6. Output: The incident number is INC0000054 and the logged in user is System Administrator
+// 5. All > Incident > All : Click on an Incident Form
+// 6. Output: The incident number is INC0000054 and the logged in user is System Administrator
